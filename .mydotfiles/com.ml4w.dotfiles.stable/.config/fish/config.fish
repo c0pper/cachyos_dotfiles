1a9777173eba928adf7767fab61320fa77a9b4d0
simo <93simonster@gmail.com>
2026-03-14 13:38:42 +0100

    added lazygit alias for dotfiles

--- .mydotfiles/com.ml4w.dotfiles.stable/.config/fish/config.fish
+++ .mydotfiles/com.ml4w.dotfiles.stable/.config/fish/config.fish
@@ -1,4 +1,5 @@
 
 fish_add_path /home/simo/.spicetify
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias lgdotfiles='lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
